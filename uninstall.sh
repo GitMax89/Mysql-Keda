0c19af190961db432286d6ed9433cf3a5a9daa7d
max <max.tvn89@gmail.com>
2022-04-20 18:37:11 +0200

    .

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+APP_DIR="./deployment"
 
 if [ -d "$APP_DIR" ]; then
    microk8s.kubectl delete secret/mysql-secrets && microk8s.kubectl delete triggerauthentication.keda.sh/keda-trigger-auth-mysql-secret && microk8s.kubectl delete scaledobject mysql-scaledobject
