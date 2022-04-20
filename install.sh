bc9c80f7b5a6203986dac70543d1eb501ccd5e9d
max <max.tvn89@gmail.com>
2022-04-20 18:28:10 +0200

    .

--- install.sh
+++ install.sh
@@ -5,7 +5,6 @@ FIRST_STEP="./deployment/config"
 SECOND_STEP="./deployment/install"
 
 if [ -d "$APP_DIR" ]; then
-   microk8s.kubectl apply -f $FIRST_STEP && sleep 8 && microk8s.kubectl apply -f $SECOND_STEP || echo la folder $APP_DIR non esiste. && exit 1
+   microk8s.kubectl apply -f $FIRST_STEP && sleep 8 && microk8s.kubectl apply -f $SECOND_STEP || echo "this folder $APP_DIR does not exist." && exit 1
 fi 
 
-echo "status exited = $?"
